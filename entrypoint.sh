d6851772bb2219989637bebd7aba772f93893c49
shall0pass <20625555+shall0pass@users.noreply.github.com>
2025-06-04 18:29:17 -0500

    cron update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Create the cron job from the environment variable
-echo "$CRON_SCHEDULE /usr/local/bin/node /app/app.js >> /var/log/cron.log 2>&1" > /etc/cron.d/mycron
+cat << 'EOF' > /app/runjob.sh
+#!/bin/bash
+/usr/local/bin/node /app/app.js >> /var/log/cron.log 2>&1
+EOF
+
+echo "$CRON_SCHEDULE root /app/runjob.sh" > /etc/cron.d/mycron
 
 # Give execution rights on the cron job
 chmod 0644 /etc/cron.d/mycron
+chmod +x /app/runjob.sh
 
 # Apply cron job
 crontab /etc/cron.d/mycron
